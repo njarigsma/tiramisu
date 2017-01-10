6519d7dcb82b5b0847f5c0ea502c0ab3c70245a3
njarigsma <nicky.jarigsma@gmail.com>
2017-01-10 15:36:11 +0100

    printing ingredients

--- tiramisu.rb
+++ tiramisu.rb
@@ -14,7 +14,7 @@ NUM_eggs = 4
 NUM_cream = 125
 NUM_mascarpone = 500
 
-ingredients [
+ingredients = [
   {name:"cream", quantity: NUM_cream},
   {name:"sugar", quantity: 75},
   {name:"eggs", quantity: NUM_eggs},
@@ -30,4 +30,21 @@ print_divider
 puts "You will need the following ingredients"
 ingredients.each do |ingredient|
 puts "#{ingredient[:quantity]} #{ingredient[:name]}"
+end
+
+#ask user if they have all the ingredients
+loop do
+  puts "\n"
+  print_divider
+  puts "Got all the ingredients you need? (Y/N)"
+  answer = gets.chomp.upcase
+  if answer == "Y"
+    puts "Great! Let's start!"
+    break
+  elsif answer == "N"
+    puts "Okay, you need to do some shopping!"
+    print_progress_bar
+  else
+    puts "I didn't ask for that, thank you. Try again."
+  end
 end
