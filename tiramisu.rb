4be14bb698a5a851925a782d4216252461fadc7b
njarigsma <nicky.jarigsma@gmail.com>
2017-01-10 16:17:30 +0100

    Iterate through the tsteps

--- tiramisu.rb
+++ tiramisu.rb
@@ -51,14 +51,14 @@ end
 
 #defining recipe steps
 steps = [
-  {description: "Klop de slagroom met 1/5 van de basterdsuiker in een ruime kom met een mixer bijna stijf. Maak de mixer schoon.", action: "whip_cream"},
+  {description: "Klop de slagroom met 1/5 van de basterdsuiker in een ruime kom met een mixer bijna stijf. Maak de mixer schoon.", action: "generic_recipe_step"},
   {description: "Splits de eieren (de eiwitten worden niet gebruikt).", action: "separate_eggs"},
-  {description: "Klop in een andere kom de eidooiers met de rest van de suiker in 5 min. tot een romig mengsel.", action: "whip_yokes"},
-  {description: "Meng de mascarpone in delen door het eidooiermengsel.", action: "add_mascarpone"},
-  {description: "Spatel de slagroom er luchtig door.", action: "add_cream"},
-  {description: "Roer in een ondiep, langwerpig schaaltje de likeur door de espresso. Doop er de helft van de lange vingers een voor een in en leg ze met de koffiekant naar boven als 1 laag in de schaal. Verdeel de helft van het mascarponemengsel erover. Maak zo nog een laag met de rest van de koffie, de lange vingers en de mascarpone.", action: "dip_dye_ladyfingers"},
-  {description: "Laat de schaal afgedekt minimaal 2 uur in de koelkast opstijven. Neem kort voor het serveren uit de koelkast. ", action: "leave_in_fridge"},
-  {description: "Bestrooi de tiramisu met behulp van een zeefje met de cacaopoeder.", action: "sprinkle_cacao"},
+  {description: "Klop in een andere kom de eidooiers met de rest van de suiker in 5 min. tot een romig mengsel.", action: "generic_recipe_step"},
+  {description: "Meng de mascarpone in delen door het eidooiermengsel.", action: "generic_recipe_step"},
+  {description: "Spatel de slagroom er luchtig door.", action: "generic_recipe_step"},
+  {description: "Roer in een ondiep, langwerpig schaaltje de likeur door de espresso. Doop er de helft van de lange vingers een voor een in en leg ze met de koffiekant naar boven als 1 laag in de schaal. Verdeel de helft van het mascarponemengsel erover. Maak zo nog een laag met de rest van de koffie, de lange vingers en de mascarpone.", action: "generic_recipe_step"},
+  {description: "Laat de schaal afgedekt minimaal 2 uur in de koelkast opstijven. Neem kort voor het serveren uit de koelkast. ", action: "generic_recipe_step"},
+  {description: "Bestrooi de tiramisu met behulp van een zeefje met de cacaopoeder.", action: "generic_recipe_step"},
 ]
 
 
@@ -83,3 +83,23 @@ def separate_eggs
     print_progress_bar
   end
 end
+
+#ask user if they're ready for the next step
+def ask_if_ready(step, index)
+  puts "Are you ready for step #{index + 1}?\n(#{step[:description]}) (Y/N)"
+  answer = gets.chomp
+  answer.upcase == 'Y'
+end
+
+#Iterating through each of the steps
+steps.each_with_index do |step, index|
+  print_divider
+  loop do
+    ready = ask_if_ready(step, index)
+    break if ready
+
+    puts "Ok, I will give you some extra time"
+    print_progress_bar
+  end
+  send(step[:action])
+  end
