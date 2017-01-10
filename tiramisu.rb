d0685d7a8fb63a1de9a3fc45d175be1f37e0d95d
njarigsma <nicky.jarigsma@gmail.com>
2017-01-10 15:45:01 +0100

    define recipe steps

--- tiramisu.rb
+++ tiramisu.rb
@@ -48,3 +48,15 @@ loop do
     puts "I didn't ask for that, thank you. Try again."
   end
 end
+
+#defining recipe steps
+steps = [
+  {description: "Klop de slagroom met 1/5 van de basterdsuiker in een ruime kom met een mixer bijna stijf. Maak de mixer schoon.", action: "whip_cream"},
+  {description: "Splits de eieren (de eiwitten worden niet gebruikt).", action: "separate_eggs"},
+  {description: "Klop in een andere kom de eidooiers met de rest van de suiker in 5 min. tot een romig mengsel.", action: "whip_yokes"},
+  {description: "Meng de mascarpone in delen door het eidooiermengsel.", action: "add_mascarpone"},
+  {description: "Spatel de slagroom er luchtig door.", action: "add_cream"},
+  {description: "Roer in een ondiep, langwerpig schaaltje de likeur door de espresso. Doop er de helft van de lange vingers een voor een in en leg ze met de koffiekant naar boven als 1 laag in de schaal. Verdeel de helft van het mascarponemengsel erover. Maak zo nog een laag met de rest van de koffie, de lange vingers en de mascarpone.", action: "dip_dye_ladyfingers"},
+  {description: "Laat de schaal afgedekt minimaal 2 uur in de koelkast opstijven. Neem kort voor het serveren uit de koelkast. ", action: "leave_in_fridge"},
+  {description: "Bestrooi de tiramisu met behulp van een zeefje met de cacaopoeder.", action: "sprinkle_cacao"},
+]
