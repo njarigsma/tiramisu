50221741a522f47aa6e636df914fba99b7009813
njarigsma <nicky.jarigsma@gmail.com>
2017-01-10 15:22:14 +0100

    define ingredients

--- tiramisu.rb
+++ tiramisu.rb
@@ -8,3 +8,18 @@ def print_progress_bar
   3.times {sleep 0.5; print "."}
   puts "\n"
 end
+
+NUM_eggs = 4
+NUM_cream = 125
+NUM_mascarpone = 500
+
+ingredients [
+  {name:"cream", quantity: NUM_cream},
+  {name:"sugar", quantity: 75},
+  {name:"eggs", quantity: NUM_eggs},
+  {name:"mascarpone", quantity: NUM_eggs},
+  {name:"amaretto", quantity: 5},
+  {name:"espresso", quantity: 200},
+  {name:"ladyfingers", quantity: 175},
+  {name:"cacao", quantity: 1},
+]
